d0374034b40de656e0ee404878e8ab305a0e40a7
Karl J. Overå <k@rl.run>
2023-10-20 15:58:43 +0200

    move config to config-dir instead of cache dir (with migration)

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,6 +1,17 @@
 import path from 'node:path'
+import fs from 'node:fs'
+
+import chalk from 'chalk'
 
 import { CACHE_DIR } from './cache.ts'
+import { log } from './log.ts'
+
+export const CONFIG_DIR = path.join(Bun.env.HOME ?? '~', '.config', 'tsm')
+
+// Dumbly just create the cache dir, we don't care, same as cache dir
+fs.mkdirSync(CONFIG_DIR, { recursive: true })
+// Migrate config from cache dir to config dir for a little while
+await migrateFromCacheToConfigDir(CACHE_DIR, CONFIG_DIR)
 
 type Config = {
     gitDir: string | undefined
@@ -12,7 +23,7 @@ const defaultConfig: Config = {
     ide: 'idea',
 }
 
-const configFile = Bun.file(path.join(CACHE_DIR, 'config.json'))
+const configFile = Bun.file(path.join(CONFIG_DIR, 'config.json'))
 
 export async function updateConfig(config: Partial<Config>): Promise<Config> {
     const currentConfig = getConfig()
@@ -25,6 +36,7 @@ export async function updateConfig(config: Partial<Config>): Promise<Config> {
 
 export async function getConfig(): Promise<Config> {
     let config: Config
+
     if (!(await configFile.exists())) {
         config = defaultConfig
         await Bun.write(configFile, JSON.stringify(defaultConfig))
@@ -36,4 +48,16 @@ export async function getConfig(): Promise<Config> {
         await Bun.write(configFile, JSON.stringify(config))
     }
     return config
+}
+
+export async function migrateFromCacheToConfigDir(cacheDir: string, configDir: string): Promise<void> {
+    const configInCacheDir = path.join(cacheDir, 'config.json')
+    const cacheConfigFile = Bun.file(configInCacheDir)
+    if (await cacheConfigFile.exists()) {
+        log(chalk.blue('\n...Found config in cache dir, migrating to config dir. This is a one-time-thing. :)\n'))
+        const cacheConfigContent = await cacheConfigFile.json<Config>()
+        const newConfigFile = Bun.file(path.join(configDir, 'config.json'))
+        await Bun.write(newConfigFile, JSON.stringify(cacheConfigContent))
+        fs.unlinkSync(configInCacheDir)
+    }
 }
