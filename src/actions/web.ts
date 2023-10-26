3ad38221286e6ca0e713e9f67391e87b63918973
Karl J. Overå <k@rl.run>
2023-10-26 16:05:45 +0200

    add apps to web command

--- src/actions/web.ts
+++ src/actions/web.ts
@@ -1,7 +1,15 @@
 import * as R from 'remeda'
 import open from 'open'
+import chalk from 'chalk'
 
 import inquirer from '../common/inquirer.ts'
+import { log } from '../common/log.ts'
+
+type Envs = {
+    dev: string
+    prod: string
+    demo?: string
+}
 
 const availablePages = {
     mock: 'https://teamsykmelding-mock.intern.dev.nav.no/',
@@ -9,9 +17,44 @@ const availablePages = {
     unleash: 'https://teamsykmelding-unleash-web.nav.cloud.nais.io/',
 }
 
-const keys = R.keys.strict(availablePages)
+type PageKeys = (typeof pageKeys)[number]
+const pageKeys = R.keys.strict(availablePages)
 
-type Keys = (typeof keys)[number]
+const availableApps = {
+    sykmeldinger: {
+        dev: 'https://www.ekstern.dev.nav.no/syk/sykmeldinger',
+        demo: 'https://sykmeldinger.ekstern.dev.nav.no/syk/sykmeldinger',
+        prod: 'https://www.nav.no/syk/sykmeldinger',
+    } satisfies Envs,
+    dinesykmeldte: {
+        dev: 'https://www.ekstern.dev.nav.no/arbeidsgiver/sykmeldte',
+        demo: 'https://dinesykmeldte.ekstern.dev.nav.no/arbeidsgiver/sykmeldte',
+        prod: 'https://www.nav.no/arbeidsgiver/sykmeldte',
+    } satisfies Envs,
+    'syk-dig': {
+        dev: 'https://syk-dig.intern.dev.nav.no',
+        demo: 'https://syk-dig.ekstern.dev.nav.no/',
+        prod: 'https://syk-dig.intern.nav.no/',
+    } satisfies Envs,
+    'sykmelder-statistikk': {
+        dev: 'https://www.ekstern.dev.nav.no/samarbeidspartner/sykmelder-statistikk',
+        demo: 'https://sykmelder-statistikk.ekstern.dev.nav.no/samarbeidspartner/sykmelder-statistikk',
+        prod: 'https://www.nav.no/samarbeidspartner/sykmelder-statistikk',
+    } satisfies Envs,
+    smregistrering: {
+        dev: 'https://smregistrering.intern.dev.nav.no/',
+        demo: 'https://smregistrering.ekstern.dev.nav.no/?oppgaveid=123',
+        prod: 'https://smregistrering.intern.nav.no/',
+    } satisfies Envs,
+    syfosmmanuell: {
+        dev: 'https://syfosmmanuell.intern.dev.nav.no/',
+        demo: 'https://syfosmmanuell.ekstern.dev.nav.no/?oppgaveid=123',
+        prod: 'https://syfosmmanuell.intern.nav.no/',
+    } satisfies Envs,
+}
+
+type AppKeys = (typeof appKeys)[number]
+const appKeys = R.keys.strict(availableApps)
 
 export async function openResource(what: string | null): Promise<void> {
     if (what != null && isPage(what)) {
@@ -19,19 +62,46 @@ export async function openResource(what: string | null): Promise<void> {
         return
     }
 
-    const { selectedPage } = await inquirer.prompt([
+    const everything = [...R.keys(availablePages), ...R.keys(availableApps).map((it) => `app: ${it}`)]
+    const { selectedRootItem } = await inquirer.prompt([
         {
             type: 'autocomplete',
-            name: 'selectedPage',
-            message: 'Hva vil du åpne?',
+            name: 'selectedRootItem',
+            message: 'What do you want to open?',
             source: async (_: unknown, input: string) =>
-                Object.keys(availablePages).filter((page) => page.includes(input ?? what ?? '')),
+                everything.filter((page) => page.includes(input ?? what ?? '')),
+        },
+    ])
+    const cleanItem = selectedRootItem.replace('app: ', '')
+
+    if (!isApp(cleanItem)) {
+        const staticPage = availablePages[cleanItem as PageKeys]
+
+        await open(staticPage)
+        return
+    }
+
+    const envs = availableApps[cleanItem]
+    const { selectedEnv } = await inquirer.prompt([
+        {
+            type: 'autocomplete',
+            name: 'selectedEnv',
+            message: `What environment for ${cleanItem} do you want to open?`,
+            source: async (_: unknown, input: string) => R.keys(envs).filter((page) => page.includes(input ?? '')),
         },
     ])
 
-    await open(availablePages[selectedPage as Keys])
+    const url = availableApps[cleanItem][selectedEnv as keyof Envs]
+
+    log(`Opening ${chalk.blue(url)}...`)
+
+    await open(url)
 }
 
-function isPage(what: string): what is Keys {
+function isPage(what: string): what is PageKeys {
     return what in availablePages
+}
+
+function isApp(what: string): what is AppKeys {
+    return what in availableApps
 }
